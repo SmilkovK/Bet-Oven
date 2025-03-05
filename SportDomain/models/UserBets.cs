94a9d45349a80c72fdd44f6d0944daf97a89206a
Атанасов Атанас <dakager@gmail.com>
2025-03-05 07:50:52 +0100

    Random pre-pull commit.

---
+++ SportDomain/models/UserBets.cs
@@ -0,0 +1,22 @@
+﻿using SportDomain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportDomain.models
+{
+    class UserBets
+    {
+        enum BetStatus
+        {
+            Active,
+            Finished
+        }
+        public List<BetValue> BetValues { get; set; }
+        public BetUser User { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
